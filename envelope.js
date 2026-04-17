5636437d4fa162dc5cc38ffd9beb210a7ad87e49
Ace <acegrantk@gmail.com>
2026-04-17 21:43:28 +0800

    Envelope

--- envelope.js
+++ envelope.js
@@ -4,12 +4,8 @@ const intro = document.getElementById("envelopeIntro");
 envelope.addEventListener("click", () => {
   envelope.classList.add("open");
 
-  // subtle "paper feel" delay
-  setTimeout(() => {
-    intro.classList.add("hide");
-  }, 1600);
-
-  setTimeout(() => {
-    intro.style.display = "none";
-  }, 2600);
+ setTimeout(() => {
+  intro.style.display = 'none';
+  window.location.href = 'main.html';
+}, 2800);
 });
